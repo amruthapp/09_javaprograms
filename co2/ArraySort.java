6e2ca5bd6a7df1938d63434a814165b147032e92
amruthapp <79830617+amruthapp@users.noreply.github.com>
2021-06-24 00:40:27 +0530

    Add files via upload

---
+++ co2/ArraySort.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public static void main(String ars[])
+{
+ int n;
+ String t;
+ Scanner sc=new Scanner(System.in);
+ System.out.println("enter the no.of strings");
+ n=sc.nextInt();
+ String arr[]=new String[n];
+ 
