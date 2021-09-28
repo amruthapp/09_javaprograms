df524cd812e81c6c08db708cc425c7dd315860e6
amruthapp <79830617+amruthapp@users.noreply.github.com>
2021-09-28 20:02:53 +0530

    Update ProductPrice.java

--- co1/prgm1/ProductPrice.java
+++ co1/prgm1/ProductPrice.java
@@ -1,3 +1,8 @@
+//1. Define a class ‘product’ with data members pcode, pname and price. Create 3 objects of
+//the class and find the product having the lowest price.
+
+
+
 class Product
   {
 	int pcode;
